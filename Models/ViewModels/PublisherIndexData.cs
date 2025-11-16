5fdaa84f84523cbd281ba5171246e8430921744e
deliapricope96 <deliapricope9@gmail.com>
2025-11-16 15:59:40 +0200

    Lab 4 punctele 1-5

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Pricope_Delia_L2.Models;
+namespace Pricope_Delia_L2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
