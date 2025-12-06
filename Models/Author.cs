db02cd35b7f6678227f658144bc04b0667d41025
deliapricope96 <deliapricope9@gmail.com>
2025-12-06 15:03:17 +0200

    Lab4- cu search bar si sortare publishers

--- Models/Author.cs
+++ Models/Author.cs
@@ -6,11 +6,17 @@ namespace Pricope_Delia_L2.Models
     {
         public int ID { get; set; }
 
-        [Display(Name = "First Name")]
-        public string? FirstName { get; set; }
-
-        [Display(Name = "Author's Last Name")]
-        public string? LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; } //navigation property
     }
 }
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -21,7 +21,7 @@ namespace Pricope_Delia_L2.Pages.Books
 
         public IActionResult OnGet()
         {
-            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID","LastName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID","FullName");
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID","PublisherName");
             var book = new Book();
             book.BookCategories = new List<BookCategory>();
